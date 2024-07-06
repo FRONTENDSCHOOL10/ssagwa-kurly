e56dd21070d222cd2f32e711bc0e3d9e333568c0
chlyun <rkdmf1306@gmail.com>
2024-07-07 01:41:55 +0900

    Feat: vite 푸터빌드 코드추가

--- vite.config.js
+++ vite.config.js
@@ -14,11 +14,7 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(root, 'index.html'),
-<<<<<<< HEAD
         footer: resolve(root, 'components/footer/footer.html'),
-=======
-        footer: resolve(root, 'components/footer/footer.html'), 
->>>>>>> e061cdf346852cb50b208c968567f2c1b9d01933
       },
     },
   },
