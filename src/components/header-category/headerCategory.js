dccda552781048eb8977f1c9a79994789f1b014e
zidoopal2@gmail.com <zidoopal2@gmail.com>
2024-07-11 08:51:24 +0900

    Feat: 헤더 - 헤더 카테고리 메뉴 아이템 컴포넌트 구현 및 적용
    - 기존 헤더에서 분리하여 카테고리 메뉴 아이템을 독립적인 컴포넌트로 구현
    - 아이콘 파일명과 타이틀을 연결하여 동적으로 생성
    - 헤더에 새로운 컴포넌트를 삽입하여 적용 완료
    - svg 이미지 호버 시 색상 변경 기능은 추후 리팩토링 시 적용 예정

---
+++ src/components/header-category/headerCategory.js
@@ -0,0 +1,54 @@
+function categoryMenuItem(shadowRoot) {
+  const menuItems = [
+    { icon: 'Gift', title: '선물하기' },
+    { icon: 'Vegetable', title: '채소' },
+    { icon: 'Fruit', title: '과일 · 견과 · 쌀' },
+    { icon: 'Seafood', title: '수산 · 해산 · 건어물' },
+    { icon: 'Meat', title: '정육 · 계란' },
+    { icon: 'Food', title: '국 · 반찬 · 메인요리' },
+    { icon: 'Salad', title: '샐러드 · 간편식' },
+    { icon: 'Oil', title: '면 · 양념 · 오일' },
+    { icon: 'Coffee', title: '생수 · 음료 · 우유 · 커피' },
+    { icon: 'Snack', title: '간식 · 과자 · 떡' },
+    { icon: 'Bread', title: '베이커리 · 치즈 · 델리' },
+    { icon: 'Health', title: '건강식품' },
+    { icon: 'Wine', title: '와인' },
+    { icon: 'TraditionalLiquor', title: '전통주' },
+    { icon: 'Detergent', title: '생활용품 · 리빙 · 캠핑' },
+    { icon: 'Cosmetics', title: '스킨케어 · 메이크업' },
+    { icon: 'Shampoo', title: '헤어 · 바디 · 구강' },
+    { icon: 'Cook', title: '주방용품' },
+    { icon: 'HomeAppliances', title: '가전제품' },
+    { icon: 'Dog', title: '반려동물' },
+    { icon: 'Baby', title: '베이비 · 키즈 · 완구' },
+    { icon: 'Travel', title: '여행 · 티켓' },
+  ];
+
+  const menuContainer = shadowRoot.querySelector('.nav__category-list');
+
+  if (!menuContainer) {
+    console.error('.nav__category-list 요소를 찾을 수 없습니다.');
+    return;
+  }
+
+  menuItems.forEach((item) => {
+    const categoryIcon = document.createElement('img');
+    const categoryTitle = document.createElement('span');
+    const categoryContainer = document.createElement('li');
+
+    categoryContainer.classList.add('nav__category__item');
+
+    categoryIcon.src = `/assets/svg/MenuIcon-${item.icon}.svg`;
+    categoryIcon.alt = item.title;
+    categoryIcon.classList.add('nav__category__item-img');
+    categoryTitle.textContent = item.title;
+    categoryTitle.classList.add('nav__category__item-title');
+
+    categoryContainer.appendChild(categoryIcon);
+    categoryContainer.appendChild(categoryTitle);
+
+    menuContainer.appendChild(categoryContainer);
+  });
+}
+
+export default categoryMenuItem;
