b5e94bcafd8329cade39b1ea73805a6de79cc9e0
zidoopal2@gmail.com <zidoopal2@gmail.com>
2024-07-10 23:24:17 +0900

    Feat 장바구니 모달 open/close 로직 구현
    - 근데 이걸 어떻게 컴포넌트화? 함수화? 시켜야할지 도저히 모르겠다
      - 다른데서 그냥 openModal(); 이렇게 쓰고싶은데!

---
+++ src/components/cartModal/cartModal.js
@@ -0,0 +1,17 @@
+const openModalButtons = document.querySelectorAll('.button--open-modal');
+const closeModalButtons = document.querySelectorAll('.button--close-modal');
+const cartModal = document.getElementById('cart-modal');
+
+openModalButtons.forEach((button) => {
+  button.addEventListener('click', () => {
+    cartModal.classList.add('is-visible');
+    cartModal.setAttribute('aria-hidden', 'false');
+  });
+});
+
+closeModalButtons.forEach((button) => {
+  button.addEventListener('click', () => {
+    cartModal.classList.remove('is-visible');
+    cartModal.setAttribute('aria-hidden', 'true');
+  });
+});
