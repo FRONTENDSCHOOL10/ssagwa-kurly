3bf06e71f793d955e2d9d51817a02c075a1bcfe5
dabini99 <djssk8475@gmail.com>
2024-07-12 15:30:17 +0900

    Feat: 할인율 0%와 빈문자일 경우 코드 작성

--- src/components/product/product.js
+++ src/components/product/product.js
@@ -1,4 +1,5 @@
-import { comma } from "/src/lib/math/index.js";
+import '/src/components/product/product.css';
+// import { comma } from "/src/lib/utils/tiger.js";
 
 
 document.addEventListener('DOMContentLoaded', function() {
@@ -6,7 +7,7 @@ document.addEventListener('DOMContentLoaded', function() {
       { 
           image: "/src/assets/images/product/product01.png", 
           name: "[풀무원] 탱탱쫄면 (4개입)", 
-          discount: "24", 
+          discount: "30", 
           price: "2980", 
           originalPrice: "24000", 
           description: "CJ즉석밥 고소한 맛의 발아 현미밥"
@@ -24,6 +25,7 @@ document.addEventListener('DOMContentLoaded', function() {
   productData.forEach((item) => {
 
       const discountPrice = realPrice(item.originalPrice, item.discount);
+      const noDiscounted = item.discount !== '0' && item.discount !== '';
 
       const template = `
             <li class="product__wrapper">
@@ -31,14 +33,14 @@ document.addEventListener('DOMContentLoaded', function() {
                     <img src="${item.image}" alt="${item.name}" />
                     <figcaption class="sr-only">상품 이미지: ${item.name}</figcaption>
                 </figure>
-                <button type="button" class="product__basket" aria-label="장바구니에 상품 담기"><img src="/src/assets/svg/Cart-1.svg" alt="장바구니 아이콘" aria-hidden="true"/>담기</button>
+                <button type="button" class="product__basket" aria-label="장바구니에 상품 담기"><img src="/public/svg/Cart-1.svg" alt="장바구니 아이콘" aria-hidden="true"/>담기</button>
                 <span class="product__delivery" aria-label="배송 설명">샛별배송</span>
                 <span class="product__title" aria-label="상품이름">${item.name}</span>
                 <div class="product__price--wrap">
-                    <div class="product__discount" aria-label="할인율">${item.discount}%</div>
-                    <div class="product__real-price" aria-label="할인 된 가격">${comma(discountPrice)}원</div>
+                    ${noDiscounted ? `<div class="product__discount" aria-label="할인율">${item.discount}%</div>` : ''}
+                    <div class="product__real-price" aria-label="가격">${noDiscounted ? `${discountPrice}원` : `${item.originalPrice}원`}</div>
                 </div>
-                <span class="product__price" aria-label="원가">${comma(item.originalPrice)}원</span>
+                <span class="product__price" aria-label="원가">${item.originalPrice}원</span>
                 <span class="product__description" aria-label="상품 설명">${item.description}</span>
                 <div class="product__tag" aria-labelledby="Karly Only">
                     <span class="tag--only tag--primary" id="Karly Only">Karly Only</span>
