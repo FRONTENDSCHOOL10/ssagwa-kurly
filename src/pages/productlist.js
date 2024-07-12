e498b489bb01344b5f020919d78e740485e72b11
chlyun <rkdmf1306@gmail.com>
2024-07-13 07:44:25 +0900

    Feat: productlistpage 마크업 및 스타일링 구현

---
+++ src/pages/productlist.js
@@ -0,0 +1,6 @@
+import '/src/styles/global.css';
+import '/src/pages/productlist.css';
+import '/src/components/header/header.js';
+import '/src/components/footer/footer.js';
+import '/src/components/paginator/paginator.css';
+import '/src/components/product_filter/filter.js';
