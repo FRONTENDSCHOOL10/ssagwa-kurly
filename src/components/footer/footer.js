b95e5ae98b182f3d0b364038a20d6e8333d1c79b
chlyun <rkdmf1306@gmail.com>
2024-07-12 20:15:12 +0900

    Modify: 칼리 -> 컬리 이름수정

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -44,8 +44,8 @@ class KurlyFooter extends HTMLElement {
             <section class="footer__company-info" aria-label="회사 정보">
               <nav>
                 <ul>
-                  <li><a href="#">칼리소개</a></li>
-                  <li><a href="#">칼리소개영상</a></li>
+                  <li><a href="#">컬리소개</a></li>
+                  <li><a href="#">컬리소개영상</a></li>
                   <li><a href="#">인재채용</a></li>
                   <li><a href="#">이용약관</a></li>
                   <li><a href="#">개인정보처리방침</a></li>
@@ -64,11 +64,11 @@ class KurlyFooter extends HTMLElement {
                 <p>팩스 : 070 - 7500 - 6098</p>
               </address>
               <ul class="company-info__sns-links" aria-label="sns링크">
-                <li><a href="https://blog.naver.com/marketkurly" target='_blank' aria-label="칼리 블로그"><img src="/svg/Blog.svg" alt="블로그아이콘"></a></li>
-                <li><a href="https://www.instagram.com/marketkurly/" target='_blank' aria-label="칼리 인스타그램"><img src="/svg/Instagram.svg" alt="인스타그램아이콘"></a></li>
-                <li><a href="https://www.facebook.com/Marketkurly/" target='_blank' aria-label="칼리 페이스북"><img src="/svg/FaceBook.svg" alt="페이스북아이콘"></a></li>
-                <li><a href="https://m.post.naver.com/marketkurly" target='_blank' aria-label="칼리 네이버 포스트"><img src="/svg/NaverPost.svg" alt="네이버포스트아이콘"></a></li>
-                <li><a href="https://www.youtube.com/channel/UCfpdjL5pl-1qKT7Xp4UQzQg" target='_blank' aria-label="칼리 유튜브"><img src="/svg/Youtube.svg" alt="유튜브아이콘"></a></li>
+                <li><a href="https://blog.naver.com/marketkurly" target='_blank' aria-label="컬리 블로그"><img src="/svg/Blog.svg" alt="블로그아이콘"></a></li>
+                <li><a href="https://www.instagram.com/marketkurly/" target='_blank' aria-label="컬리 인스타그램"><img src="/svg/Instagram.svg" alt="인스타그램아이콘"></a></li>
+                <li><a href="https://www.facebook.com/Marketkurly/" target='_blank' aria-label="컬리 페이스북"><img src="/svg/FaceBook.svg" alt="페이스북아이콘"></a></li>
+                <li><a href="https://m.post.naver.com/marketkurly" target='_blank' aria-label="컬리 네이버 포스트"><img src="/svg/NaverPost.svg" alt="네이버포스트아이콘"></a></li>
+                <li><a href="https://www.youtube.com/channel/UCfpdjL5pl-1qKT7Xp4UQzQg" target='_blank' aria-label="컬리 유튜브"><img src="/svg/Youtube.svg" alt="유튜브아이콘"></a></li>
               </ul>
             </section>
           </div>
@@ -76,7 +76,7 @@ class KurlyFooter extends HTMLElement {
             
               <a href="/" target='_blank'>
                 <img src="/svg/isms.svg" alt="">
-                <p>[인증범위] 마켓칼리 쇼핑몰 서비스 개발 운영<br />(심사받지 않은 물리적 인프라 제외)<br /> [유효기간] 2022.01.19 ~ 2025.01.18</p>
+                <p>[인증범위] 마켓컬리 쇼핑몰 서비스 개발 운영<br />(심사받지 않은 물리적 인프라 제외)<br /> [유효기간] 2022.01.19 ~ 2025.01.18</p>
               </a>
               <a href="/" target='_blank'>
                 <img src="/svg/logo_privacy.svg" alt="">
@@ -95,8 +95,8 @@ class KurlyFooter extends HTMLElement {
         </div>
         <div class="footer__bottom">
           <div class="footer__subinner">
-            <small>마켓칼리에서 판매되는 상품 중에는 마켓칼리에 입점한 개별 판매자가 판매하는 마켓플레이스(오픈마켓) 상품이 포함되어 있습니다.<br />
-            마켓플레이스(오픈마켓) 상품의 경우 칼리는 통신판매중개자로서 통신판매의 당사자가 아닙니다. 칼리는 해당 상품의 주문, 품질, 교환/환불 등 의무와 책임을 부담하지 않습니다.</small>
+            <small>마켓컬리에서 판매되는 상품 중에는 마켓컬리에 입점한 개별 판매자가 판매하는 마켓플레이스(오픈마켓) 상품이 포함되어 있습니다.<br />
+            마켓플레이스(오픈마켓) 상품의 경우 컬리는 통신판매중개자로서 통신판매의 당사자가 아닙니다. 컬리는 해당 상품의 주문, 품질, 교환/환불 등 의무와 책임을 부담하지 않습니다.</small>
             <small>© KURLY CORP. ALL RIGHTS RESERVED</small>
           </div>
         </div>
