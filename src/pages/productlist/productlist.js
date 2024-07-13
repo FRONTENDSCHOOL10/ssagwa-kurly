915f2519593f733ad8bfbaaa2a859400250d05fd
chlyun <rkdmf1306@gmail.com>
2024-07-14 02:24:07 +0900

    Feat: productlistpage 마크업 스타일링

--- src/pages/productlist/productlist.js
+++ src/pages/productlist/productlist.js
@@ -1,6 +1,4 @@
 import '/src/styles/global.css';
-import '/src/pages/productlist.css';
+import '/src/components/pagenatior/pagenatior.css';
 import '/src/components/header/header.js';
 import '/src/components/footer/footer.js';
-import '/src/components/paginator/paginator.css';
-import '/src/components/product_filter/filter.js';
