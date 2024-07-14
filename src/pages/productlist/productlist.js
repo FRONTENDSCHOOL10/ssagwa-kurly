31b0d7a4a2de657c01412a4d81941f44261e36bc
chlyun <rkdmf1306@gmail.com>
2024-07-14 16:42:49 +0900

    Feat: filter기능추가

--- src/pages/productlist/productlist.js
+++ src/pages/productlist/productlist.js
@@ -2,3 +2,4 @@ import '/src/styles/global.css';
 import '/src/components/pagenatior/pagenatior.css';
 import '/src/components/header/header.js';
 import '/src/components/footer/footer.js';
+import '/src/components/product_filter/filter.js';
