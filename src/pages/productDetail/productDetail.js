f82953cb1800d48f8ffc95c96c8c76cc44975baf
jaehwan-space <ma2582m@naver.com>
2024-07-15 01:40:37 +0900

    Feat: 상품 설명 부분 마크업 및 스타일링

---
+++ src/pages/productDetail/productDetail.js
@@ -0,0 +1,9 @@
+import '/src/pages/productDetail/productDetail.css';
+
+import '/src/styles/global.css';
+import '/src/components/header/header.js';
+import '/src/components/footer/footer.js';
+import '/src/components/button/button.css';
+import initializeStepper from '/src/components/stepper/stepper.js';
+
+initializeStepper(document.querySelector('.stepper'));
