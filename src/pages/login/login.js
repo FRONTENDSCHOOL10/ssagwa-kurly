47972bab2d57131c4f2172be31682ce7d4cf8322
dabini99 <djssk8475@gmail.com>
2024-07-14 19:14:34 +0900

    Feat: 로그인 가능하게 만들기

--- src/pages/login/login.js
+++ src/pages/login/login.js
@@ -1,6 +1,43 @@
 import '/src/components/input/input.css';
 import '/src/pages/login/login.css';
 import '/src/components/button/button.css';
+import pb from '/src/api/pocketbase.js'
+import { getNode } from "/src/lib/dom";
+import { getStorage,setStorage } from "/src/lib/utils/storage.js";
 
 
 //setDocumentTitle.js로 페이지 이름 정해두기
+
+const loginButton = getNode('.login');
+
+function handleLogin(e){
+  e.preventDefault();
+
+  console.log('로그인 시도');
+
+  // const id = 'kimnunu@naver.com';
+  // const pw = 'dkssud123@123';
+
+  const id = getNode('#userEmail').value;
+  const pw = getNode('#userPw').value;
+
+  pb.collection('users').authWithPassword(id,pw)
+  .then(async()=>{
+    
+    const {model,token} = await getStorage('pocketbase_auth');
+
+    setStorage('auth',{
+      isAuth: !!model,
+      user: model,
+      token
+    })
+    
+    alert('로그인 완료! 메인페이지로 이동합니다.');
+    location.href = '/index.html'
+
+  },()=>{
+    alert('인증된 사용자가 아닙니다.')
+  })
+}
+
+loginButton.addEventListener('click',handleLogin);
