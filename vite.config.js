9dc4bdfccbf7750ed763d99a6a63b44abcfe82d9
jaehwan-space <ma2582m@naver.com>
2024-07-15 13:42:48 +0900

    Modify: register 빌드 경로 수정

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'index.html'),
-        register: resolve(__dirname, '/src/pages/register/index.html'),
+        register: resolve(__dirname, './src/pages/register/index.html'),
       },
     },
   },
