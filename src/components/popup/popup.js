c6dbf628d75c82ca371cf2caa6a110c7d9b4aed6
dabini99 <djssk8475@gmail.com>
2024-07-16 20:38:14 +0900

    feat: 닫기 버튼을 누르면 팝업과 배경이 hidden됨

---
+++ src/components/popup/popup.js
@@ -0,0 +1,12 @@
+import '/src/components/popup/popup.css';
+import '/src/styles/global.css';
+
+
+// 닫기를 클릭하면 hidden 클래스가 붙어야함
+const btnClose = document.querySelector('.btn-2');
+  const modalClose = () => {
+    const modalOverlay = document.querySelector('.modal-overlay'); 
+    modalOverlay.classList.add("hidden");
+  }
+
+  btnClose.addEventListener('click', modalClose);
