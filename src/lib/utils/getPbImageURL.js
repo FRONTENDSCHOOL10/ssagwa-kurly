c5a52448a1ef51ab365a015cae3b9e1382f2c379
jaehwan-space <ma2582m@naver.com>
2024-07-16 14:03:06 +0900

    Modify: 스토리지에 맞는 이미지 파일명 기입

--- src/lib/utils/getPbImageURL.js
+++ src/lib/utils/getPbImageURL.js
@@ -1,4 +1,4 @@
-export function getPbImageURL(item,fileName = 'photo'){
+export function getPbImageURL(item,fileName = 'productImg'){
   return `${import.meta.env.VITE_PB_API}/files/${item.collectionId}/${item.id}/${item[fileName]}`
 }
 
