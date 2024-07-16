c223853c6200b7a6adfded26b13f9cec4d5495bd
dabini99 <djssk8475@gmail.com>
2024-07-16 20:44:44 +0900

    Style: 코드 포멧팅

--- src/components/popup/popup.js
+++ src/components/popup/popup.js
@@ -4,9 +4,10 @@ import '/src/styles/global.css';
 
 // 닫기를 클릭하면 hidden 클래스가 붙어야함
 const btnClose = document.querySelector('.btn-2');
-  const modalClose = () => {
-    const modalOverlay = document.querySelector('.modal-overlay'); 
-    modalOverlay.classList.add("hidden");
-  }
+const modalClose = () => {
+  const modalOverlay = document.querySelector('.modal-overlay'); 
+  modalOverlay.classList.add("hidden");
+}
 
-  btnClose.addEventListener('click', modalClose);
+btnClose.addEventListener('click', modalClose);
+
