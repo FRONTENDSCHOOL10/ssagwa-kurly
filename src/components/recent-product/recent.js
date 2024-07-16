2b5aec267f72625a4826b7fceb77b2cba6b19fb6
dabini99 <djssk8475@gmail.com>
2024-07-16 11:36:31 +0900

    feat: 유효기간, 저장가능한 아이템 갯수 설정

--- src/components/recent-product/recent.js
+++ src/components/recent-product/recent.js
@@ -1,1 +1,6 @@
 import '/src/components/recent-product/recent.css';
+import { setStorage, getStorage } from "/src/lib/utils/storage.js";
+
+const LATELY_VIEW_ITEM_EXPIRATION_DATE = 1; //유효기간 1일
+const LATELY_VIEW_MAX_SAVE_COUNT = 5;  // 저장할 수 있는 아이템 갯수 5개!
+
