b6977ac6005ffe704c0a2d1210b53b211c6ee0fa
jaehwan-space <ma2582m@naver.com>
2024-07-16 14:08:19 +0900

    Feat: callback 함수 매개변수에 추가

--- src/components/modal/modal.js
+++ src/components/modal/modal.js
@@ -1,6 +1,6 @@
 import '/src/components/modal/modal.css';
 
-export default function viewModal(text) {
+export default function viewModal(text, callback) {
   // 모달 컨테이너 div를 생성합니다.
   const modalContainer = document.createElement('div');
   modalContainer.classList.add('modalContainer');
@@ -19,6 +19,11 @@ export default function viewModal(text) {
   // 닫기 버튼에 이벤트 리스너를 추가합니다.
   document.querySelectorAll('.modalCloseButton').forEach(function(btn){
     btn.addEventListener('click', function () {
+      // callback 함수 실행
+      if (typeof callback === 'function') {
+        callback();
+      }
+
       document.querySelectorAll('.modalContainer').forEach(function(modal) {
         document.body.removeChild(modal);
       });
