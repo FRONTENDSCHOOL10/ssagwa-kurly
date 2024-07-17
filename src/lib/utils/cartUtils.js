6659a9346928beab816fa539bd40f3775148b347
zidoopal2@gmail.com <zidoopal2@gmail.com>
2024-07-18 00:33:11 +0900

    Feat: 장바구니 관련 유틸리티 함수 구현
    - 모달 없이 툴팁만 뜨는 로직 addToCart
    
    - 장바구니 모달이 뜬 후 addToCart함수를 호출하는 addToCartwithModal

---
+++ src/lib/utils/cartUtils.js
@@ -0,0 +1,51 @@
+import { setStorage, getStorage } from './storage.js';
+import { openCartTooltip } from '/src/components/tooltip/tooltip.js';
+import openCartModal from '/src/components/cart-modal/cartModal.js';
+
+/**
+ * addToCart()
+ * '장바구니에 담기' 버튼 클릭 시 장바구니 모달을 띄우지 않고
+ * 툴팁 알림만 띄우고 장바구니에 상품을 담는 함수
+ */
+export function addToCart(product, quantity) {
+  let cart = getStorage('cart');
+
+  if (!Array.isArray(cart)) {
+    cart = [];
+  }
+
+  const existingItem = cart.find((item) => item.id === product.id);
+  const isDuplicate = !!existingItem;
+
+  if (isDuplicate) {
+    existingItem.quantity += quantity;
+  } else {
+    product.quantity = quantity;
+    cart.push(product);
+  }
+
+  setStorage('cart', cart);
+  openCartTooltip(product, isDuplicate);
+}
+
+/**
+ * addToCartwithModal()
+ * '장바구니에 담기' 버튼 클릭 시 장바구니 모달을 띄워 수량을 저장한 후
+ * addToCart 함수 호출
+ */
+export function addToCartwithModal(e) {
+  const button = e.target.closest('button');
+  if (!button) return;
+
+  let product;
+  try {
+    product = JSON.parse(button.dataset.product);
+  } catch (e) {
+    console.error('유효한 JSON 데이터가 아니에요 ☹:', button.dataset.product);
+    return;
+  }
+
+  openCartModal(product, (quantity) => {
+    addToCart(product, quantity);
+  });
+}
