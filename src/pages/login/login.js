68bab610891477f65c8500728c0d481f51e8933a
jaehwan-space <ma2582m@naver.com>
2024-07-18 16:31:29 +0900

    Refactor: a 링크 리팩토링 - index.html 삭제 - 상품 리스트 페이지 접속 링크 url 파라미터 추가

--- src/pages/login/login.js
+++ src/pages/login/login.js
@@ -36,7 +36,7 @@ function handleLogin(e) {
           token,
         });
 
-        location.href = '/index.html';
+        location.href = '/';
       },
       () => {
         viewModal('아이디 또는 비밀번호를 확인해 주세요.');
